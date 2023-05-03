e012c9e1abc89ef8f85256dc22042be335acd11d
Halobrobro <Halobrobro@mail.ru>
2023-05-03 22:33:31 +0700

    menu

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace lab3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a;
+			double b;
+			double total;
+			Console.WriteLine("Введите А: ");
+			a = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите B: ");
+			b = Convert.ToDouble(Console.ReadLine());
+			char znak;
+			Console.WriteLine("Введите знак для реализации операции:");
+			Console.WriteLine("(+)");
+			Console.WriteLine("(-)");
+			Console.WriteLine("(*)");
+			Console.WriteLine("(/)");
+			znak = Convert.ToChar(Console.ReadLine());
+			switch (znak)
+			{ }
+		}
+	}
+}
