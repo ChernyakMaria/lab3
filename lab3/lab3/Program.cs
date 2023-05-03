cd24eec4b9827b5f7641b8055a19cd971bd7a83d
Halobrobro <Halobrobro@mail.ru>
2023-05-03 22:37:04 +0700

    b

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,26 +1,15 @@
 ﻿using System;
-namespace lab3
+namespace Menu
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			double a;
 			double b;
-			double total;
-			Console.WriteLine("Введите А: ");
-			a = Convert.ToDouble(Console.ReadLine());
 			Console.WriteLine("Введите B: ");
 			b = Convert.ToDouble(Console.ReadLine());
-			char znak;
-			Console.WriteLine("Введите знак для реализации операции:");
-			Console.WriteLine("(+)");
-			Console.WriteLine("(-)");
-			Console.WriteLine("(*)");
-			Console.WriteLine("(/)");
-			znak = Convert.ToChar(Console.ReadLine());
-			switch (znak)
-			{ }
+
 		}
+
 	}
 }
